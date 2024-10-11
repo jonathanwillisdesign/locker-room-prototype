2116ad492fad9a1b37438e5fad32c38eb6907149
jonathanwillisdesign <jonathanw@bigmotive.com>
2024-10-11 18:33:36 +0100

    added in maze snippet

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import { ThemeProvider } from "@/components/theme-provider"
 import Header from '@/components/header'
 import { Toaster } from '@/components/ui/toaster'
+import Script from 'next/script' // Import Script from Next.js
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -30,6 +31,34 @@ export default function RootLayout({
           {children}
           <Toaster />
         </ThemeProvider>
+        {/* Add the external script here */}
+        <Script
+          id="maze-universal-loader"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function (m, a, z, e) {
+                var s, t;
+                try {
+                  t = m.sessionStorage.getItem('maze-us');
+                } catch (err) {}
+
+                if (!t) {
+                  t = new Date().getTime();
+                  try {
+                    m.sessionStorage.setItem('maze-us', t);
+                  } catch (err) {}
+                }
+
+                s = a.createElement('script');
+                s.src = z + '?apiKey=' + e;
+                s.async = true;
+                a.getElementsByTagName('head')[0].appendChild(s);
+                m.mazeUniversalSnippetApiKey = e;
+              })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', 'f4f5840d-9486-487c-8e86-b4e645d87587');
+            `,
+          }}
+        />
       </body>
     </html>
   )
